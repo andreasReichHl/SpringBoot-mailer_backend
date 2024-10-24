14161dbb776cfcdb96da2c1e9f96c50f74bec90e
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-24 16:10:11 +0200

    update code

--- src/main/java/de/example/mailer/controller/MailController.java
+++ src/main/java/de/example/mailer/controller/MailController.java
@@ -1,6 +1,8 @@
 package de.example.mailer.controller;
 
 import de.example.mailer.services.MailService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,7 @@ public class MailController {
         this.mailService = mailService;
     }
 
+    @PostMapping
     public void sendMail(){
         mailService.sendMail("silversurfer@ohne.surfboard", "Kauf dir mal ne Board", ""
         + "Hallo Silver Surfer, darfst du dich überhaupt Silver Surfer nennen, obwohl gar kein Surfboard hast?");
